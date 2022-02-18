45b8d4ba36a462308afe6eeba15b0459a2bd39ff
MiltonAltSan <66339220+MiltonAltSan@users.noreply.github.com>
2022-02-18 14:19:34 -0700

    Update train.py

--- train.py
+++ train.py
@@ -65,9 +65,10 @@ def train_net(net,
     ''')
 
     # 4. Set up the optimizer, the loss, the learning rate scheduler and the loss scaling for AMP
-    optimizer = optim.RMSprop(net.parameters(), lr=learning_rate, weight_decay=1e-8, momentum=0.9)
-    #scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)  # goal: maximize Dice score    
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=2)  # goal: maximize Dice score
+    optimizer = optim.Adam(net.parameters(), lr=learning_rate, weight_decay=1e-8)
+    #optimizer = optim.RMSprop(net.parameters(), lr=learning_rate, weight_decay=1e-8, momentum=0.9)
+    scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)  # goal: maximize Dice score    
+    #scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=2)  # goal: maximize Dice score
     grad_scaler = torch.cuda.amp.GradScaler(enabled=amp)
     criterion = nn.CrossEntropyLoss()
     global_step = 0
